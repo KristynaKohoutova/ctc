af3e76d70cbb5987f928177d9ba40ec8ab2f25e8
Kristyna_Kohoutova <kristyna.kohoutova@tul.cz>
2022-02-24 09:05:51 +0100

    adding function logic

--- cv01/fibonacci.go
+++ cv01/fibonacci.go
@@ -5,7 +5,12 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-
+	var a int = 0
+	var b int = 1
+	return func() int {
+		a, b = a+b, a
+		return b
+	}
 }
 
 func main() {
